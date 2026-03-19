ca2629fdb851f6b51284e342c5cdc44dc277249c
Manincircle <manincircle@163.com>
2026-03-19 23:50:31 +0800

    0029

---
+++ 0029/0029.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+        
+        long long a = labs(dividend);
+        long long b = labs(divisor);
+        long long res = 0;
+        
+        while (a >= b) {
+            long long temp = b, multiple = 1;
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            a -= temp;
+            res += multiple;
+        }
+        
+        bool negative = (dividend > 0) ^ (divisor > 0);
+        return negative ? -res : res;
+    }
+};
